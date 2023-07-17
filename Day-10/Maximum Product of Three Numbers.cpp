2e4192a7f2edd82a1f8dc534b032e726a949bf62
Parth Madan <101553190+coolmadan@users.noreply.github.com>
2023-07-18 00:35:03 +0530

    Create Maximum Product of Three Numbers.cpp

---
+++ Day-10/Maximum Product of Three Numbers.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int option1=nums[nums.size()-1]*nums[nums.size()-2]*nums[nums.size()-3];
+        int option2=nums[0]*nums[1]*nums[nums.size()-1];
+        
+        return max(option1,option2);
+    }
+};
